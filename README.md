ed42848586f1da014b56c23331b7158bd7f1b5d8
Jacob Thompson <jakethompson77@hotmail.co.uk>
2022-11-03 16:13:34 +0000

    added calculator.rb

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# RB_101
+# Launch School: RB_101
+
+### This repo tracks my personal progress through the RB_101 course at [Launch School].
+
+[Launch School]: https://launchschool.com
---
+++ lesson_2/calculator.rb
@@ -0,0 +1,27 @@
+# asks the user for two numbers
+# asks the user for an operation to perform
+# perform the operation on the two numbers
+# output the result
+
+# note: all methods are prefixed with their module and suffixed with their parameters for learning purposes
+
+Kernel.puts("Welcome to Calculator")
+
+Kernel.puts("What's the first number?")
+number1 = Kernel.gets().chomp().to_i
+
+Kernel.puts("What's the second number?")
+number2 = Kernel.gets().chomp().to_i
+
+Kernel.puts("What's the operation?: \n1 = add   2 = subtract \n3 = multiply   4 = divide")
+operator = Kernel.gets().chomp().to_i
+
+case operator
+  when 1 then result = number1 + number2
+  when 2 then result = number1 - number2
+  when 3 then result = number1 * number2
+  when 4 then result = number1 / number2
+  else Kernel.puts("Uh-Oh... something went wrong")
+end
+
+Kernel.puts("==> Result = #{result}")
